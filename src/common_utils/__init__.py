0ed3b0c7ac8c73135382540c9cc501769722de09
Minh Tran <minhtr94@gmail.comm>
2024-10-09 23:18:19 +0100

    email sender now working

---
+++ src/common_utils/__init__.py
@@ -0,0 +1,1 @@
+from common_utils.emailer import send
---
+++ src/common_utils/emailer.py
@@ -0,0 +1,47 @@
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from common_utils.credentials import apply_default_sender_detail
+
+
+@apply_default_sender_detail
+def send(sender: str, password: str, recipients_list: list, subject: str, message: str):
+
+    if not sender:
+        raise Exception(
+            "Missing sender! Check for google_email_app_username in .env file or add a sender gmail email"
+        )
+
+    if not password:
+        raise Exception(
+            "Missing password! Check for google_email_app_password in .env file or add a password"
+        )
+
+    msg = MIMEMultipart()
+    msg["Subject"] = subject
+    msg["From"] = sender
+
+    msg.attach(
+        MIMEText(
+            f"""\
+        <html>
+            <head>
+            </head>
+            <body>
+                <br>{"<br><br>".join(message)}<br>
+            </body>
+        </html>
+        """,
+            "html",
+        )
+    )
+
+    with smtplib.SMTP(host="smtp.gmail.com", port=587) as server:
+        server.starttls()
+        server.ehlo()
+        server.login(
+            user=msg["From"],
+            password=password,
+        )
+        server.sendmail(from_addr=sender, to_addrs=recipients_list, msg=msg.as_string())
+        server.quit()
