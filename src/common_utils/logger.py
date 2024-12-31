ab3641a067a742087345a73b1004f4d799a76071
Minh Tran <minhtr94@gmail.comm>
2024-12-31 10:30:59 +0000

    add remove handlers functions

--- src/common_utils/logger.py
+++ src/common_utils/logger.py
@@ -16,3 +16,12 @@ def create_logger(logging_level=logging.CRITICAL, log_file_path="logging.log"):
     logger.addHandler(streamHandler)
     logger.addHandler(fileHandler)
     return logger
+
+
+def remove_log_handlers(logger_name=__name__):
+    logger = logging.getLogger(name=logger_name)
+    if not logger.handlers:
+        return
+    for hdlr in logger.handlers:
+        hdlr.close()
+        logger.removeHandler(hdlr)
