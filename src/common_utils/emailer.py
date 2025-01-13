6a8072060a0a5fc324dc49253bcb5d57fbc3a74c
Minh Tran <minhtr94@gmail.comm>
2025-01-13 01:48:21 +0000

    singledispatch method for emailer.send

--- src/common_utils/emailer.py
+++ src/common_utils/emailer.py
@@ -2,14 +2,43 @@ import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 import os
+from functools import singledispatch
 
 
+def _send_email(from_addr, to_addrs, password, msg):
+    try:
+        with smtplib.SMTP(host="smtp.gmail.com", port=587) as server:
+            server.starttls()
+            server.ehlo()
+            server.login(
+                user=msg["From"],
+                password=password,
+            )
+            server.sendmail(from_addr=from_addr, to_addrs=to_addrs, msg=msg.as_string())
+            server.quit()
+
+    except Exception as e:
+        raise e
+
+
+@singledispatch
 def send(
+    message,
+    subject=None,
+    recipients=None,
+    username=os.getenv("google_email_app_username"),
+    password=os.getenv("google_email_app_password"),
+):
+    raise NotImplementedError("Unsupported message type")
+
+
+@send.register
+def _send_simple_string_message(
+    message: str,
+    subject: str = None,
+    recipients: list = None,
     username: str = os.getenv("google_email_app_username"),
     password: str = os.getenv("google_email_app_password"),
-    recipients_list: list = None,
-    subject: str = None,
-    message: str = None,
 ):
     if not username:
         raise Exception(
@@ -40,22 +69,43 @@ def send(
         )
     )
 
-    with smtplib.SMTP(host="smtp.gmail.com", port=587) as server:
-        server.starttls()
-        server.ehlo()
-        server.login(
-            user=msg["From"],
-            password=password,
+    _send_email(
+        from_addr=username,
+        to_addrs=recipients,
+        password=password,
+        msg=msg,
+    )
+
+
+@send.register
+def _send_mimemultipart_message(
+    message: MIMEMultipart,
+    subject: str = None,
+    recipients: list = None,
+    username: str = os.getenv("google_email_app_username"),
+    password: str = os.getenv("google_email_app_password"),
+):
+    if not username:
+        raise Exception(
+            "Missing sender! Check for google_email_app_username in .env file or add a sender gmail email"
         )
-        server.sendmail(
-            from_addr=username, to_addrs=recipients_list, msg=msg.as_string()
+
+    if not password:
+        raise Exception(
+            "Missing password! Check for google_email_app_password in .env file or add a password"
         )
-        server.quit()
+
+    msg = message
+    msg["Subject"] = subject
+    msg["From"] = username
+
+    _send_email(
+        from_addr=username,
+        to_addrs=recipients,
+        password=password,
+        msg=msg,
+    )
 
 
 if __name__ == "__main__":
-    send(
-        recipients_list=["minhtr94@gmail.com"],
-        subject="testing",
-        message="test",
-    )
+    send("test", subject="testing", recipients=["minhtr94@gmail.com"])  # this works
