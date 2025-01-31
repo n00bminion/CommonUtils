a0879b0e9ac33f682bc6b44ae06f377b0a317c98
Minh Tran <minhtr94@gmail.comm>
2025-01-31 19:00:40 +0000

    updated version of sql_handler

--- src/common_utils/__init__.py
+++ src/common_utils/__init__.py
@@ -1,3 +1,8 @@
 from common_utils.emailer import send
 from common_utils.logger import create_logger
 from common_utils.config_handler import get_config
+
+from dotenv import find_dotenv, load_dotenv
+
+dotenv_file = find_dotenv(usecwd=True)
+load_dotenv(dotenv_file)
--- src/common_utils/sql_handler.py
+++ src/common_utils/sql_handler.py
@@ -4,204 +4,196 @@ import os
 from pathlib import Path
 
 
-def _conditional_parser(conditionals):
-    """
-    Function to parse the conditional
-    paramter for function the SQL.truncate_table()
-    """
+class DatabaseConnection:
+    def __init__(self, **kwargs):
+        # for now we only deal with sqlite db file so
+        # just need to get the file path and file name for the db file.
+        self.database_path = kwargs.get("database_path", os.getcwd())
+        self.database_file = kwargs.get("database_file", os.getenv("database_file"))
 
-    if conditionals == {}:
-        return ""
+        print(self.database_file)
 
-    def _parse(column, conditional):
+        if self.database_path and self.database_file:
+            self._connection = self._connect_to_db(
+                database_path=self.database_path, database_file=self.database_file
+            )
+            return
 
-        prefix = f" AND {column}"
+        raise Exception(
+            f"Connection was not established, check params passed in:\n {str(kwargs)}"
+        )
 
-        if isinstance(conditional, dict):
+    def _connect_to_db(self, database_path, database_file):
+        """
+        Function to point to a DB file and access its content
+        (will also generate DB file if not exists)
 
-            if not len(conditional):
-                return ""
+        """
+        ## if the path exists, connection will
+        assert database_file, "Must include db file name"
 
-            dict_conditional = []
-            for key, value in conditional.items():
+        if os.path.isdir(database_path):
+            return sqlite3.connect(Path(database_path, database_file))
 
-                if isinstance(value, str):
-                    value = f"'{value}'"
+    def __enter__(self):
+        return self._connection
 
-                dict_conditional.append(f"{prefix} {key} {value}")
+    def __exit__(self, type, value, tb):
+        self._connection.close()
 
-                return " ".join(dict_conditional)
+    @staticmethod
+    def _conditional_parser(conditionals):
+        """
+        Function to parse the conditional
+        paramter for function the SQL.truncate_table()
+        """
 
-        if isinstance(conditional, list):
-            if all((isinstance(item, int) for item in conditional)):
-                f'{prefix} IN ({",".join(map(str, conditional))})'
+        if conditionals == {}:
+            return ""
 
-            return f"{prefix} IN ('{"','".join(conditional)}')"
+        def _parse(column, conditional):
 
-        if isinstance(conditional, int):
-            return f"{prefix} = {conditional}"
+            prefix = f" AND {column}"
 
-        return f"{prefix} = '{conditional}'"
+            if isinstance(conditional, dict):
 
-    ## remove the first 'and '
-    return (
-        "WHERE"
-        + "".join(
-            [
-                _parse(column, conditional)
-                for column, conditional in conditionals.items()
-            ]
-        )[4:]
-    )
+                if not len(conditional):
+                    return ""
 
+                dict_conditional = []
+                for key, value in conditional.items():
 
-def _default_select(table, conditional):
-    return f" SELECT * FROM {table} {_conditional_parser(conditional)}"
+                    if isinstance(value, str):
+                        value = f"'{value}'"
 
+                    dict_conditional.append(f"{prefix} {key} {value}")
 
-def _connect_to_db(database_path, database_file):
-    """
-    Function to point to a DB file and access its content
-    (will also generate DB file if not exists)
+                    return " ".join(dict_conditional)
 
-    """
-    ## if the path exists, connection will
-    assert database_file, "Must include db file name"
+            if isinstance(conditional, list):
+                if all((isinstance(item, int) for item in conditional)):
+                    f'{prefix} IN ({",".join(map(str, conditional))})'
 
-    if os.path.isdir(database_path):
-        return sqlite3.connect(Path(database_path, database_file))
+                return f"{prefix} IN ('{"','".join(conditional)}')"
 
+            if isinstance(conditional, int):
+                return f"{prefix} = {conditional}"
 
-def select_into_dataframe(
-    query=None,
-    table=None,
-    conditional=None,
-    database_path=os.getcwd(),
-    database_file=os.getenv("database_file"),
-):
-    """
-    Pass "select" query as argument and returns
-    data in a DataFrame. Here's a simple example:
+            return f"{prefix} = '{conditional}'"
 
-    ''' select * from sqlite_master '''
+        ## remove the first 'and '
+        return (
+            "WHERE"
+            + "".join(
+                [
+                    _parse(column, conditional)
+                    for column, conditional in conditionals.items()
+                ]
+            )[4:]
+        )
 
-    """
-    conn = _connect_to_db(database_path, database_file)
-    if table and (conditional is not None) and not query:
-        query = _default_select(table, conditional)
+    @staticmethod
+    def _default_select(table, conditional):
+        return f" SELECT * FROM {table} {DatabaseConnection._conditional_parser(conditional)}"
 
-    if not query:
-        raise Exception(f"query and/or {table=} and {conditional=} was not supplied")
+    def select_into_dataframe(self, query=None, table=None, conditional=None):
+        """
+        Pass "select" query as argument and returns
+        data in a DataFrame. Here's a simple example:
 
-    df = pd.read_sql_query(str(query), conn)
+        ''' select * from sqlite_master '''
 
-    conn.close()
-    return df
+        """
 
+        if table and (conditional is not None) and not query:
+            query = DatabaseConnection._default_select(table, conditional)
 
-def exec_sql_query(
-    query,
-    database_path=os.getcwd(),
-    database_file=os.getenv("database_file"),
-):
-    """
-    Used to execute query in the SQLite DB (i.e. Create table, drop table etc.).
-    Below is an example, 1st query creating a test table and 2nd query is to drop test table (copy and try it out):
+        if not query:
+            raise Exception(
+                f"query and/or {table=} and {conditional=} was not supplied"
+            )
 
-    - Create a new table
-    '''CREATE TABLE IF NOT EXISTS test_table (id integer PRIMARY KEY, name text , begin_date text, end_date text);'''
+        df = pd.read_sql_query(str(query), self._connection)
 
-    - Dropping a table
-    '''DROP TABLE IF EXISTS test_table;'''
+        return df
 
-    """
-    conn = _connect_to_db(database_path, database_file)
-    conn.cursor().executescript(str(query))
-    conn.commit()
-    conn.close()
+    def exec_sql_query(self, query):
+        """
+        Used to execute query in the SQLite DB (i.e. Create table, drop table etc.).
+        Below is an example, 1st query creating a test table and 2nd query is to drop test table (copy and try it out):
 
+        - Create a new table
+        '''CREATE TABLE IF NOT EXISTS test_table (id integer PRIMARY KEY, name text , begin_date text, end_date text);'''
 
-def insert_data_frame_to_sql(
-    dataframe,
-    table,
-    database_path=os.getcwd(),
-    database_file=os.getenv("database_file"),
-    if_exists="append",
-):
-    """
-    Insert data from dataframe source table and SQLite target table, column names need to match
+        - Dropping a table
+        '''DROP TABLE IF EXISTS test_table;'''
 
-    category = pd.DataFrame([
-                        [1,'b','f'],
-                        [2,'a','o'],
-                        [3,'r','z']
-                    ])
-    category.columns = ['a','b','c']
+        """
 
-    insert_data_frame_to_sql(category,'Category')
-    """
-    conn = _connect_to_db(database_path, database_file)
-    assert isinstance(dataframe, pd.DataFrame), "Use dataframe as data source"
-    dataframe.to_sql(name=table, con=conn, index=False, if_exists=if_exists)
+        self._connection.cursor().executescript(str(query))
+        self._connection.commit()
 
+    def insert_data_frame_to_sql(
+        self,
+        dataframe,
+        table,
+        if_exists="append",
+    ):
+        """
+        Insert data from dataframe source table and SQLite target table, column names need to match
 
-def truncate_table(
-    table,
-    conditional=None,
-    database_path=os.getcwd(),
-    database_file=os.getenv("database_file"),
-):
-    """
-    Clear out/truncate the table
-    Leave condition param empty if you want
-    to truncate the whole table else pass through
-    a dictionary with key being the column
-    and the values as list of the items
-    in the row that you would like to delete from
+        category = pd.DataFrame([
+                            [1,'b','f'],
+                            [2,'a','o'],
+                            [3,'r','z']
+                        ])
+        category.columns = ['a','b','c']
 
-    Parameter
-        table <<str>>
-        comditional <<dict>>
+        insert_data_frame_to_sql(category,'Category')
+        """
+        assert isinstance(dataframe, pd.DataFrame), "Use dataframe as data source"
+        dataframe.to_sql(
+            name=table, con=self._connection, index=False, if_exists=if_exists
+        )
 
-    Example
-        conditional = {
-            'symbol':['TSLA','AAPL','RBLX'],
-            'country':'United States',
-        }
-        truncate_table('freetrade_security_raw',conditional)
-    """
-    conditional = "" if conditional == None else _conditional_parser(conditional)
-    query = " DELETE FROM {} {}".format(table, conditional)
-    exec_sql_query(query, database_path, database_file)
+    def truncate_table(self, table, conditional=None):
+        """
+        Clear out/truncate the table
+        Leave condition param empty if you want
+        to truncate the whole table else pass through
+        a dictionary with key being the column
+        and the values as list of the items
+        in the row that you would like to delete from
 
+        Parameter
+            table <<str>>
+            comditional <<dict>>
 
-def list_sql_objects(
-    database_path=os.getcwd(),
-    database_file=os.getenv("database_file"),
-):
-    """Return a list table of a objects in the DB"""
-    return [
-        i
-        for i in select_into_dataframe(
-            query=" SELECT name FROM sqlite_master WHERE type IN ('table','view') ORDER BY name",
-            database_path=database_path,
-            database_file=database_file,
-        ).name
-    ]
+        Example
+            conditional = {
+                'symbol':['TSLA','AAPL','RBLX'],
+                'country':'United States',
+            }
+            truncate_table('freetrade_security_raw',conditional)
+        """
+        conditional = (
+            ""
+            if conditional == None
+            else DatabaseConnection._conditional_parser(conditional)
+        )
+        self.exec_sql_query(" DELETE FROM {} {}".format(table, conditional))
 
+    def list_sql_objects(self):
+        """Return a list table of a objects in the DB"""
+        return self.select_into_dataframe(
+            query=" SELECT name FROM sqlite_master WHERE type IN ('table','view') ORDER BY name"
+        ).name.to_list()
 
-def get_sql_columns(
-    table,
-    database_path=os.getcwd(),
-    database_file=os.getenv("database_file"),
-):
-    """Return a list of columns for the required table"""
-    assert isinstance(table, str), "table needs to a string"
-    return [
-        i
-        for i in select_into_dataframe(
-            query=f"SELECT * FROM {table} limit 0",
-            database_path=database_path,
-            database_file=database_file,
-        ).columns
-    ]
+    def get_sql_columns(self, table):
+        """Return a list of columns for the required table"""
+        assert isinstance(table, str), "table needs to a string"
+        return list(
+            self.select_into_dataframe(
+                query=f"SELECT * FROM {table} limit 0",
+            ).columns
+        )
