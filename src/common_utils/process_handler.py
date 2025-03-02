6c4d55602c61149667a1b82dc4a6932d072c9f07
Minh Tran <minhtr94@gmail.comm>
2025-03-02 14:46:54 +0000

    rename process funcs

--- src/common_utils/process_handler.py
+++ src/common_utils/process_handler.py
@@ -1,13 +1,13 @@
 from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
 
 
-def useMultiThread(func, *arg):
+def use_multi_thread(func, *arg):
     with ThreadPoolExecutor() as tpe:
         res = tpe.map(func, *arg)
     return [i for i in res]
 
 
-def useMultiProcess(func, *arg):
+def use_multi_process(func, *arg):
     with ProcessPoolExecutor() as ppe:
         res = ppe.map(func, *arg)
     return [i for i in res]
