fb36cc6425ef0baba608728b4799c4b97abf6aa0
Minh Tran <minhtr94@gmail.comm>
2025-07-29 02:53:52 +0100

    added function to add github action scheduler as well as create github action workflow folder if it doesn't exist

---
+++ src/common_utils/scheduler.py
@@ -0,0 +1,29 @@
+import os
+import shutil
+from pathlib import Path
+
+GITHUB_WORKFLOW_DIR = Path(".github", "workflows")
+YAML_DIR = Path("yaml")
+
+
+def add_github_action_folder():
+    if not os.path.exists(GITHUB_WORKFLOW_DIR):
+        os.makedirs(GITHUB_WORKFLOW_DIR)
+
+
+def add_github_action_scheduler():
+    """Add a GitHub Action Scheduler file to the repository if it does not exist."""
+
+    add_github_action_folder()
+
+    scheduler_file = "github-action-scheduler.yaml"
+
+    if not os.path.isfile(GITHUB_WORKFLOW_DIR / scheduler_file):
+        shutil.copy(
+            src=YAML_DIR / scheduler_file,
+            dst=GITHUB_WORKFLOW_DIR / scheduler_file,
+        )
+
+
+if __name__ == "__main__":
+    add_github_action_scheduler()
