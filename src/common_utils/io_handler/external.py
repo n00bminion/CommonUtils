bb9e84ef07fc0f536254412080797e9e75fbc6f2
Minh Tran <minhtr94@gmail.comm>
2025-08-24 23:12:57 +0100

    added post_request

--- src/common_utils/io_handler/external.py
+++ src/common_utils/io_handler/external.py
@@ -13,3 +13,17 @@ def get_request(*args, **kwargs):
         raise e
 
     return response
+
+
+def post_request(*args, **kwargs):
+    """
+    Wrapper around requests.post function that
+    deals with status code and returns the response object
+    """
+    response = requests.post(*args, **kwargs)
+    try:
+        response.raise_for_status()
+    except requests.exceptions.HTTPError as e:
+        raise e
+
+    return response
