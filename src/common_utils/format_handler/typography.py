10163985a3cfc46182ffd48a77059ad8f6753c8f
Minh Tran <minhtr94@gmail.comm>
2025-11-15 18:55:42 +0100

    allowed for default value when convert from large number abbreviation to scalar

--- src/common_utils/format_handler/typography.py
+++ src/common_utils/format_handler/typography.py
@@ -32,8 +32,17 @@ def _get_from_global_dict(value, global_dict):
     return derived_value
 
 
-def convert_large_number_str_to_numeric_scaler(value):
-    return _get_from_global_dict(value=value, global_dict=_STR_NUM_SCALE_TO_NUM)
+def convert_large_number_str_to_numeric_scalar(value, return_value_when_fail=None):
+    try:
+        return _get_from_global_dict(value=value, global_dict=_STR_NUM_SCALE_TO_NUM)
+    except ValueError:
+        if return_value_when_fail is None:
+            raise ValueError(
+                f"Unable to convert {value} to a numeric scalar, there are 2 options to proceed:\n"
+                "1. Set the 'return_value_when_fail' parameter (to 1 maybe?) to allow a default value to be returned.\n"
+                "2. Update the global variable '_STR_NUM_SCALE_TO_NUM' in common_utils/format_handler/typography to account for the new mapping."
+            )
+        return return_value_when_fail
 
 
 def convert_string_to_bool(value):
@@ -70,5 +79,7 @@ def convert_pascal_to_snake_case(text):
 
 
 if __name__ == "__main__":
-    print(convert_camel_to_snake_case("someTextToTestHere"))
-    print(convert_camel_to_snake_case("_someTextToTestHere"))
+    # print(convert_camel_to_snake_case("someTextToTestHere"))
+    # print(convert_camel_to_snake_case("_someTextToTestHere"))
+
+    print(convert_large_number_str_to_numeric_scalar("qewas", return_value_when_fail=1))
